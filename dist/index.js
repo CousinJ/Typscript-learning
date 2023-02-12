f2e2be9ed73e6dd25f2447237a4d0d386f318683
Jason Davis <Jasonodavis323@gmail.com>
2023-02-12 13:07:45 -0700

    union types and more functions

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
 let sales = 123512;
-let course = 'TypeScript';
+let course = "TypeScript";
 let is_published = true;
-let my_document = { name: 'jason', level: 55 };
+let my_document = { name: "jason", level: 55 };
 let my_array = [];
 let user = [1, "Jason"];
 let variable1 = undefined;
@@ -18,7 +18,7 @@ var Size;
 })(Size || (Size = {}));
 let mySize = Size.MEDIUM;
 console.log(mySize);
-my_array.forEach(n => n.toString);
+my_array.forEach((n) => n.toString);
 function render(doc) {
     console.log(doc);
 }
@@ -27,13 +27,13 @@ function doSomething(param) {
 }
 render(my_document);
 const my_character = {
-    class: 'Mage',
-    level: 1
+    class: "Mage",
+    level: 1,
 };
 const my_triangle = {
     sideALength: 3,
     sideBLength: 3,
-    sideCLength: 3
+    sideCLength: 3,
 };
 const my_square = {
     sideALength: 3,
@@ -41,4 +41,24 @@ const my_square = {
     sideCLength: 3,
     sideDLength: 3,
 };
+const amazingFunc = (param) => {
+    if (param) {
+        console.log("valid parameter");
+    }
+    else {
+        console.log("NOT valid parameter");
+    }
+};
+amazingFunc(false);
+amazingFunc(59);
+function func1(a, b, c) {
+    let sum = a + b + (c || 0);
+    return sum;
+}
+function func2(a, b, c = 5) {
+    let product = a * b * c;
+    return product;
+}
+console.log(func1(5, 5, 5), func1(5, 5));
+console.log(func2(2, 2, 2), func2(2, 2));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-
-//delcaration with types 
+//delcaration with types
 let sales: number = 123_512;
-let course: string = 'TypeScript';
+let course: string = "TypeScript";
 let is_published: boolean = true;
-let my_document: {name: string, level: number} = {name: 'jason', level: 55}
-let my_array: number[] = []
-let user: [number, string] = [1, "Jason"]
+let my_document: { name: string; level: number } = { name: "jason", level: 55 };
+let my_array: number[] = [];
+let user: [number, string] = [1, "Jason"];
 //special types
 let variable1: unknown = undefined;
 let variable2: any = undefined;
@@ -13,71 +12,99 @@ let variable3: never;
 let variable4: null = null;
 let variable5: undefined = undefined;
 //enum practice
-enum Size { SMALL = 's', MEDIUM = 'm', LARGE = 'l' }
+enum Size {
+  SMALL = "s",
+  MEDIUM = "m",
+  LARGE = "l",
+}
 
 let mySize = Size.MEDIUM;
 
-console.log(mySize)
+console.log(mySize);
 
 //note the methods you can use on data types.
-my_array.forEach( n => n.toString)
+my_array.forEach((n) => n.toString);
 
 function render(doc: object) {
-    console.log(doc);
+  console.log(doc);
 }
 
-
-//parameter types and return types 
+//parameter types and return types
 function doSomething(param: number): void {
-    console.log(param);
-
-    
+  console.log(param);
 }
 
-
-render(my_document)
-
+render(my_document);
 
 //type aliases
 type startingClass = string;
 type startingLevel = number;
 //using a type alias with type alias
 type startingCharacter = {
-    class: startingClass,
-    level: startingLevel
-}
+  class: startingClass;
+  level: startingLevel;
+};
 
 const my_character: startingCharacter = {
-    class: 'Mage',
-    level: 1
-}
+  class: "Mage",
+  level: 1,
+};
 
 //interfaces  (only apply to objects)
 
 interface triangle {
-    sideALength: number,
-    sideBLength: number,
-    sideCLength: number
+  sideALength: number;
+  sideBLength: number;
+  sideCLength: number;
 }
 
 const my_triangle: triangle = {
-
-    sideALength: 3,
-    sideBLength: 3,
-    sideCLength: 3
-    
-}
+  sideALength: 3,
+  sideBLength: 3,
+  sideCLength: 3,
+};
 
 //extending interfaces
 
 interface sqaure extends triangle {
-    sideDLength: number
-
+  sideDLength: number;
 }
 
 const my_square: sqaure = {
-    sideALength: 3,
-    sideBLength: 3,
-    sideCLength: 3,
-    sideDLength: 3,
+  sideALength: 3,
+  sideBLength: 3,
+  sideCLength: 3,
+  sideDLength: 3,
+};
+//union types
+//...more than one type
+
+const amazingFunc = (param: number | boolean) => {
+  if (param) {
+    console.log("valid parameter");
+  } else {
+    console.log("NOT valid parameter");
+  }
+};
+amazingFunc(false);
+amazingFunc(59);
+
+//default paramters and optional parameters
+
+//OPTIONAL
+function func1(a: number, b: number, c?: number): number {
+  //must set the optional paramteter up with an alternative
+  let sum = a + b + (c || 0);
+  return sum;
 }
+//DEFAULT
+function func2(a: number, b: number, c: number = 5): number {
+  let product = a * b * c;
+
+  return product;
+}
+//function one
+console.log(func1(5, 5, 5), func1(5, 5));
+
+//function two
+console.log(func2(2, 2, 2), func2(2, 2));
