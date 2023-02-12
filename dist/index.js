73e4f4b66babc7ce3ac54a69711c0b3ca4d06256
Jason Davis <Jasonodavis323@gmail.com>
2023-02-11 18:44:08 -0700

    interfaces and aliases

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,15 @@
 "use strict";
-let level = 1;
-if (level > 5) {
-    console.log('max level reached');
-}
-else {
-    console.log(level);
-}
 let sales = 123512;
 let course = 'TypeScript';
 let is_published = true;
 let my_document = { name: 'jason', level: 55 };
 let my_array = [];
 let user = [1, "Jason"];
+let variable1 = undefined;
+let variable2 = undefined;
+let variable3;
+let variable4 = null;
+let variable5 = undefined;
 var Size;
 (function (Size) {
     Size["SMALL"] = "s";
@@ -24,5 +22,23 @@ my_array.forEach(n => n.toString);
 function render(doc) {
     console.log(doc);
 }
+function doSomething(param) {
+    console.log(param);
+}
 render(my_document);
+const my_character = {
+    class: 'Mage',
+    level: 1
+};
+const my_triangle = {
+    sideALength: 3,
+    sideBLength: 3,
+    sideCLength: 3
+};
+const my_square = {
+    sideALength: 3,
+    sideBLength: 3,
+    sideCLength: 3,
+    sideDLength: 3,
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ let is_published: boolean = true;
 let my_document: {name: string, level: number} = {name: 'jason', level: 55}
 let my_array: number[] = []
 let user: [number, string] = [1, "Jason"]
-
+//special types
+let variable1: unknown = undefined;
+let variable2: any = undefined;
+let variable3: never;
+let variable4: null = null;
+let variable5: undefined = undefined;
 //enum practice
 enum Size { SMALL = 's', MEDIUM = 'm', LARGE = 'l' }
 
@@ -23,7 +28,7 @@ function render(doc: object) {
 
 
 //parameter types and return types 
-function doSomethhing(param: number): void {
+function doSomething(param: number): void {
     console.log(param);
 
     
@@ -31,3 +36,48 @@ function doSomethhing(param: number): void {
 
 
 render(my_document)
+
+
+//type aliases
+type startingClass = string;
+type startingLevel = number;
+//using a type alias with type alias
+type startingCharacter = {
+    class: startingClass,
+    level: startingLevel
+}
+
+const my_character: startingCharacter = {
+    class: 'Mage',
+    level: 1
+}
+
+//interfaces  (only apply to objects)
+
+interface triangle {
+    sideALength: number,
+    sideBLength: number,
+    sideCLength: number
+}
+
+const my_triangle: triangle = {
+
+    sideALength: 3,
+    sideBLength: 3,
+    sideCLength: 3
+    
+}
+
+//extending interfaces
+
+interface sqaure extends triangle {
+    sideDLength: number
+
+}
+
+const my_square: sqaure = {
+    sideALength: 3,
+    sideBLength: 3,
+    sideCLength: 3,
+    sideDLength: 3,
+}
